7123d2754f6fc871f660de6ebcf2ed3bedfce28a
Leon <tahjib.leon399@gmail.com>
2024-04-26 02:10:06 +0600

    backend completed

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { authMiddleware } = require("../middleware/authMiddleware");
-const { getBalance } = require("../service/account");
+const { getBalance, transferMoney } = require("../service/account");
 
 const accountRouter = express.Router();
 
 accountRouter.get("/balance", authMiddleware, getBalance);
+accountRouter.post("/transfer", authMiddleware, transferMoney);
 
 
 module.exports = accountRouter;
